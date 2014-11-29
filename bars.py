0fd41ec68a0b0af245ff1f3583e9d21d37fe1a54
mishef10 <mishef10@mail.ru>
2014-11-29 19:10:23 +1000

    Выполнено задание 10

--- bars.py
+++ bars.py
@@ -1,6 +1,6 @@
 import pygame, os, sys
 class Block():#Класс Блок
-    def __init__(self, cords, width, height, color,copyColor):
+    def __init__(self, cords, width, height, color,):
         """
         Метод конструктор
         """
@@ -12,7 +12,7 @@ class Block():#Класс Блок
         self.color=color
         self.second_color = (255,0,0)
         self.draw()
-        self.originalColor=copyColor
+        self.originalColor=color
         self.drag=False
         self.deformation=False
 
@@ -76,6 +76,7 @@ class Block():#Класс Блок
             # self.drag_and_drop(e)
             elif self.collidePoint(e.pos):
                 self.drag = True
+                return self
 
 
 
@@ -109,7 +110,7 @@ class Block():#Класс Блок
         self.draw()
         if self.w <= 10:
             self.w=10
-        if self.h <= 10:
+        elif self.h <= 10:
             self.h=10
 
 
@@ -125,10 +126,17 @@ display = pygame.display.set_mode((400, 400)) #создание окна
 """
 Содание объектов
 """
-first=Block((100, 10), 80, 100, (0,0,0), (0,0,0))
-second=Block((100, 100), 90, 40, (0,101,20), (0,101,20))
-third=Block((23, 150), 45, 40, (101,101,101),(101,101,101))
-
+first=Block((100, 10), 80, 100, (0,0,0), )
+second=Block((100, 100), 90, 40, (0,101,20))
+third=Block((23, 150), 45, 40, (101,101,101))
+fourth=Block((40, 66,), 8, 12, (12, 103, 234))
+fifth=Block((34, 76,), 28, 12, (1, 103, 234))
+sixth=Block((44, 86,), 38, 12, (12, 103, 234))
+seventh=Block((90, 96,), 48, 12, (12, 103, 234))
+eighth=Block((104, 106,), 28, 12, (12, 103, 234))
+ninth=Block((29, 116,), 58, 12, (12, 103, 234))
+tenth=Block((88, 236,), 68, 12, (12, 103, 234))
+objects = [first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth]
 while True:  #главный цикл программы
     for e in pygame.event.get(): #цикл обработки очереди событий окна
         if e.type == pygame.KEYDOWN:
@@ -138,33 +146,32 @@ while True:  #главный цикл программы
             sys.exit()
 
         if e.type == pygame.MOUSEBUTTONDOWN:
-            # print(e)
-            first.events(e)
-            second.events(e)
-            third.events(e)
+            for el in objects:
+                if el.events(e):
+                    print("click on", el)
+                    helper=el
+                    objects.remove(el)
+                    objects.append(el)
+                    break
 
 
         if e.type == pygame.MOUSEBUTTONUP:
-            first.events(e)
-            second.events(e)
-            third.events(e)
+            for el in objects:
+                el.events(e)
+
 
         if e.type == pygame.MOUSEMOTION:
+            for el in objects:
+                el.events(e)
 
-            first.events(e)
-            second.events(e)
-            third.events(e)
-            # print(e)
-            # print(first.moveON(e.rel,))
             pass
         """
         Отображение объектов
         """
 
     display.fill((0,0,100))
-    first.render(display)
-    second.render(display)
-    third.render(display)
+    for el in objects:
+        el.render(display)
     pygame.display.flip()
 
 
